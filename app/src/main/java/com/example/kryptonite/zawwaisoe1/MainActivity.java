cc61966f11a9959f42a4409bb812270e49fcac40
zawwaizoe <rohinjar456+1@gmail.com>
2017-11-07 01:00:16 -0800

    Initial commit

---
+++ app/src/main/java/com/example/kryptonite/zawwaisoe1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.kryptonite.zawwaisoe1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private  void ss(){
+        int oo = 9;
+        int pp=99;
+    }
+}
